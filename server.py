423a980bde4837b72514dce03c508edf21780001
Erin Leeds <erin@erinleeds.com>
2020-08-13 01:22:51 +0000

    End of Day 8/12

--- server.py
+++ server.py
@@ -14,25 +14,28 @@ def homepage():
     return render_template('homepage.html')
 
 @app.route('/', methods = ['POST'])
+def create_user():
 
+    
+prijt
 @app.route('/login')
 def login_page():
     
     return render_template('login.html')
-    
+
 @app.route('/login', methods = ['POST'])
 def handle_login():
 
     login_email = request.form.get('login_email')
     login_password = request.form.get('login_password')
-    user = crud.get_user_by_email(login_email)
-    if login_password == user.password:  
-        session['current_user'] = user.user_id
-        flash(f'Logged in as {login_email}')
-        return redirect('/')
-    else:
-        flash('Wrong password')
-        return redirect('/login')
+    # user = crud.get_user_by_email(login_email)
+    # if login_password == user.password:  
+    #     session['current_user'] = user.user_id
+    #     flash(f'Logged in as {login_email}')
+    #     return redirect('/')
+    # else:
+    #     flash('Wrong password')
+    #     return redirect('/login')
 
 if __name__ == '__main__':
     connect_to_db(app)
