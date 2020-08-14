a584eb791de6e2791ad0bdf4b66a7cf3aa3c9aff
Erin Leeds <erin@erinleeds.com>
2020-08-14 22:25:05 +0000

    Handled user login and create account components

--- server.py
+++ server.py
@@ -1,33 +1,54 @@
-from flask import (Flask, render_template, request, flash, session,
-                   redirect)
+from flask import Flask, render_template, request, flash, session, redirect, jsonify
 from model import connect_to_db
 import crud
 from jinja2 import StrictUndefined
+import json
 
 app = Flask(__name__)
 app.secret_key = "abc"
 app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
-def homepage():
+def root():
 
-    return render_template("homepage.html")
-
-# @app.route('/', methods = ['POST'])
-# def create_user():
+    return render_template("root.html")
 
 
-@app.route('/login')
-def login_page():
+@app.route('/api/create-user', methods = ["POST"] )
+def create_user():
+    """Create User"""
+    data = request.get_json()
+    print(data)
+    fname = data['fname']
+    lname = data['lname']
+    email = data['email']
+    password = data['password']
+    if crud.get_user_by_email(email):
+        return jsonify("account exists")
+    else:
+        user = crud.create_user(fname, lname, email, password)
+        print(user)
+        return jsonify("ok")
 
-    return render_template("login.html")
+@app.route('/api/login', methods = ['POST']) 
+def login():
+    """Login User"""
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
 
-@app.route('/location-search')
-def location_search():
-    return render_template('location_search.html')
-
-
+    user = crud.get_user_by_email(email) 
+    
+    if not user.email:
+        return jsonify("Username not found")
+    elif password != user.password:
+       return jsonify("Password not found")
+    else:
+        print(user)
+        session["current_user"] = user.user_id
+        return jsonify("sucess")
 
 if __name__ == '__main__':
     connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
+
